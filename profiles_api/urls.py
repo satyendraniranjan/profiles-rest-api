199e2ea6bd03e86305b25f69d299d60b9710a29b
sat.n760@gmail.com <sat.n760@gmail.com>
2019-08-22 01:02:19 +0530

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -9,6 +9,7 @@ router.register('profile', views.UserProfileViewSet, base_name='hello-viewset')
 
 urlpatterns=[
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -7,6 +7,9 @@ from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 
 class HelloApiView(APIView):
@@ -119,3 +122,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
