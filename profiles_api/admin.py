5e614f189608f36b87f5b06e72cdfb3c3dafb75a
sat.n760@gmail.com <sat.n760@gmail.com>
2019-08-23 20:43:22 +0530

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from profiles_api.models import UserProfile, UserProfileManager
+from profiles_api.models import UserProfile, UserProfileManager, ProfileFeedItem
 # Register your models here.
 
 
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
